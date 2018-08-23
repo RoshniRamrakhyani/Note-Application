3c77bfd8c69ac5bb5f4c6b1d967da16791db0f48
Nadeem Bhati <nadeemb53@gmail.com>
2018-08-23 13:36:17 +0530

    initial commit by second collaborator

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AddNotePage } from '../add-note/add-note';
+import { NoteService } from '../../providers/note-service/note-service';
+import { Note } from '../../model/note.model';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+ notes: Promise<Note[]>;
+
+  constructor(public navCtrl: NavController, private noteService: NoteService) {
+
+  }
+  ionViewWillEnter(){
+  
+    this.notes=this.getAllNotes();
+  }
+addNote(){
+  this.navCtrl.push(AddNotePage);
+}
+getAllNotes(){
+  return this.noteService.getAllNotes();
+}
+}
